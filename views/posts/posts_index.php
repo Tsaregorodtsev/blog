9b8ca90296f6bdc3703a4fb6baca247a34b53c3c
Tsaregorodtsev <v.tsaregorodtsev@khk.ee>
2018-03-14 11:35:27 +0200

    timestamp

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,8 +1,8 @@
 <?foreach($posts as $post):?><div class="span8">
-    <h1><?=$post['posts_subjects']?></h1>
-    <p><?=$post['posts_text']?></p>
+    <h1><?=$post['post_subject']?></h1>
+    <p><?=$post['post_text']?></p>
     <div>
-        <span class="badge badge-success">Posted 2012-08-02 20:47:04</span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
+        <span class="badge badge-success"><?=$post['post_created']?></span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
     </div>
-</div>
-<?endforeach?>:
+    </div>
+<?endforeach?>
